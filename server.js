2b465d5fb7ee50a5e228a9ba49bc7d78c2256e25
Ryan Hightower <ryan@ryanhightower.com>
2015-04-16 22:34:04 -0600

    Added server configuration and Setup basic route to views/index.html
    Installed body-parser to convert raw son to js objects.
    Updated dependencies.
    Added body-parser to express in expressConfig.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var server = express();
 
+// configure server (assets folder, middleware, etc.)
+require('./server/expressConfig')(server, express);
+
+
+server.get('/', function(req, res){
+  res.sendFile('index.html', {root: server.settings.views});
+});
+
+// load api endpoints
+// require('./server/api.js')(server);
+
 server
 	.listen(3000, function(){
 		console.log('Server running on 3000. Bottoms up...');
