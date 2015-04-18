70b700098f09a77f6f39c6cda9112ecbe5813917
Ryan Hightower <ryan@ryanhightower.com>
2015-04-18 12:19:40 -0600

    added brewerydb-node and updated beerAPI

--- server/api/beer.js
+++ server/api/beer.js
@@ -3,16 +3,22 @@ var brewdb = new BreweryDb('3c5b60e0236d76112a44f5f381eb38f7');
 
 module.exports = function(app){
   
-  app.get('/beers', function(req, res){
+  // return all beers
+  app.get('/beer', function(req, res){
   // Make brewdb call here
     
+  	brewdb.beers(function(err, data){
+  		if(err) throw err;
 
+  		res.send(data);
+  	});
   });
 
+  // return a single beer
   app.get('/beers/:id', function(req, res){
     var beerId = parseInt(req.params.id, 10);
     // Make brewdb call here
-   
+	   
 
   });
 };
